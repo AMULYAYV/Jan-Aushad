859c2cc45b39cf140df10a2c7f5faf0966c5b806
AMULYAYV <yvamulya@gmail.com>
2026-05-19 00:42:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/janaushadh1/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.zenhealth.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            resources.getIdentifier(
+                "activity_main",
+                "layout",
+                packageName
+            )
+        )
+
+        webView = findViewById(
+            resources.getIdentifier(
+                "webView",
+                "id",
+                packageName
+            )
+        )
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://zenhealth-509796132053.asia-southeast1.run.app")
+
+        // ✅ Back button handling fix
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        finish()
+                    }
+                }
+            }
+        )
+    }
+}
